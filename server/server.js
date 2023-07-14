2e3d5e9131b2fce0147667d72f2280933e0b11eb
Anubhav singh <anubhavsinghnit5132@gmail.com>
2023-07-14 11:44:53 +0530

    created chatsModel and messageModel

--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ const express = require("express");
 require("dotenv").config();
 const dbConfig=require('./config/dbConfig')
 const app = express();
+const chatsRoute = require("./routes/chatsRoute");
 
 const port = process.env.PORT || 5000;
 
 const usersRoute=require('./routes/usersRoute')
 app.use(express.json());
-app.use("/api/users",usersRoute)
+app.use("/api/users",usersRoute);
+app.use("/api/chats", chatsRoute);
+
 app.listen(port,()=>{console.log(`server ok running on port ${port}`)})
