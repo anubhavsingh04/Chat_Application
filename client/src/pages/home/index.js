feab2a7feb30529b7e008c3729900fd1b0f86554
Anubhav singh <anubhavsinghnit5132@gmail.com>
2023-07-16 23:54:42 +0530

    Realtime messages with typing and online badge

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,55 +1,44 @@
-import React,{useEffect, useState} from 'react'
-import ChatArea from './components/ChatArea'
-import UserSearch from './components/UserSearch'
-import UsersList from './components/UsersList'
-import { useSelector } from 'react-redux'
-import {io} from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import ChatArea from "./components/ChatArea";
+import UserSearch from "./components/UserSearch";
+import UsersList from "./components/UsersList";
+import { useSelector } from "react-redux";
+import { io } from "socket.io-client";
 
+const socket = io("http://localhost:5000");
 
 function Home() {
-  const socket=io("http://localhost:5000");
-  const [searchKey, setSearchKey] = useState("")
-  const {selectedChat,user}=useSelector((state=>state.userReducer))
-
+  const [searchKey, setSearchKey] = useState("");
+  const { selectedChat, user } = useSelector((state) => state.userReducer);
+  const [onlineUsers, setOnlineUsers] = React.useState([]);
   useEffect(() => {
     // join the room
-    if(user)
-    {
-      socket.emit("join-room",user._id);
-      // send new message to receipent 
-      socket.emit("send-message",{
-        text:"Hi arjun this is from John",
-        sender:user._id,
-        receipent:"64b037fd5ac9d6f247dca69a",
-      })
+    if (user) {
+      socket.emit("join-room", user._id);
+      socket.emit("came-online", user._id);
 
-      // receive message from arjun
-      socket.on("receive-message",(data)=>{
-        console.log(data);
+      socket.on("online-users-updated", (users) => {
+        setOnlineUsers(users);
       });
     }
-  }, [user])
-  
+  }, [user]);
+
   return (
     <div className="flex gap-5">
-        {/* 1st part   user search , userslist/chatlist */}
-        <div className="w-96 ">
-            <UserSearch
-              searchKey={searchKey} 
-              setSearchKey={setSearchKey}
-             />
-            <UsersList 
-              searchKey={searchKey}
-            />
-        </div>
+      {/* 1st part   user search , userslist/chatlist */}
+      <div className="w-96 ">
+        <UserSearch searchKey={searchKey} setSearchKey={setSearchKey} />
+        <UsersList searchKey={searchKey} socket={socket} onlineUsers={onlineUsers}/>
+      </div>
 
-        {/* 2nd part chat box */}
-        {selectedChat && (<div className="w-full">
-            <ChatArea
-              socket={socket} />
-        </div>)}
+      {/* 2nd part chat box */}
+      {selectedChat && (
+        <div className="w-full">
+          <ChatArea socket={socket} />
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
